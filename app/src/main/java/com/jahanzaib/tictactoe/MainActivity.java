3f312de2d61796fdba3bdbf613969c70c2d5866f
Jahanzaib Hussain <m.jahanzaibhussain@gmail.com>
2016-09-16 22:44:02 +0500

    some modification in java and xml

--- app/src/main/java/com/jahanzaib/tictactoe/MainActivity.java
+++ app/src/main/java/com/jahanzaib/tictactoe/MainActivity.java
@@ -4,19 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9,restart;
     int turn;
-    public void resetAll(View view){
-        endGame();
-    }
+    TextView score_0;
+    TextView score_x;
+    int x = 0;
+    int o = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        score_0 = (TextView) findViewById(R.id.score_o);
+        score_x = (TextView) findViewById(R.id.score_x);
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
         b3 = (Button) findViewById(R.id.b3);
@@ -26,8 +31,23 @@ public class MainActivity extends AppCompatActivity {
         b7 = (Button) findViewById(R.id.b7);
         b8 = (Button) findViewById(R.id.b8);
         b9 = (Button) findViewById(R.id.b9);
-
+        restart = (Button) findViewById(R.id.button);
         turn = 1;
+
+        restart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b1.setText("");
+                b2.setText("");
+                b3.setText("");
+                b4.setText("");
+                b5.setText("");
+                b6.setText("");
+                b7.setText("");
+                b8.setText("");
+                b9.setText("");
+            }
+        });
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +58,14 @@ public class MainActivity extends AppCompatActivity {
                         b1.setText("X");
                     } else if (turn == 2) {
                         turn = 1;
-                        b2.setText("O");
+                        b1.setText("O");
                     }
             }
                 endGame();
             }
         });
+
+
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -174,12 +196,13 @@ public class MainActivity extends AppCompatActivity {
 
         });
     }
+
     public void endGame(){
         String  a, b, c, d, e, f, g, h, i;
         boolean end = false;
 
         a = b1.getText().toString();
-        b = b3.getText().toString();
+        b = b2.getText().toString();
         c = b3.getText().toString();
 
         d = b4.getText().toString();
@@ -194,34 +217,106 @@ public class MainActivity extends AppCompatActivity {
         //for X player
         if (a.equals("X") && b.equals("X") && c.equals("X")){
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (d.equals("X") && e.equals("X") && f.equals("X")) {
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (g.equals("X") && h.equals("X") && i.equals("X")){
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (a.equals("X") && d.equals("X") && g.equals("X")){
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (b.equals("X") && e.equals("X") && h.equals("X")) {
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (c.equals("X") && f.equals("X") && i.equals("X")){
-            Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (a.equals("X") && e.equals("X") && i.equals("X")){
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (c.equals("X") && e.equals("X") && g.equals("X")){
             Toast.makeText(MainActivity.this, "Player X Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(x++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
         }
 
@@ -229,47 +324,153 @@ public class MainActivity extends AppCompatActivity {
 
         if (a.equals("O") && b.equals("O") && c.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_0.setText(score);
+            resetAllButton();
             end = true;
         }
         if (d.equals("O") && e.equals("O") && f.equals("O")) {
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_x.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (g.equals("O") && h.equals("O") && i.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (a.equals("O") && d.equals("O") && g.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (b.equals("O") && e.equals("O") && h.equals("O")) {
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (c.equals("O") && f.equals("O") && i.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (a.equals("O") && e.equals("O") && i.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+                score = String.valueOf(o++);
+
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
         }
         if (c.equals("O") && e.equals("O") && g.equals("O")){
             Toast.makeText(MainActivity.this, "Player O Wins", Toast.LENGTH_LONG).show();
+            String score = "";
+
+            score = String.valueOf(o++);
+
+            score_0.setText(score);
+            resetAllButton();
             end = true;
+            try{
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
+
         }
 
-        if (end) {
-            b1.setEnabled(false);
-            b2.setEnabled(false);
-            b3.setEnabled(false);
-            b4.setEnabled(false);
-            b5.setEnabled(false);
-            b6.setEnabled(false);
-            b7.setEnabled(false);
-            b8.setEnabled(false);
-            b9.setEnabled(false);
+        if (end == false && b1.getText() != "" && b2.getText() != "" && b3.getText() != "" && b4.getText() != ""
+                && b5.getText() != "" && b6.getText() != "" && b7.getText() != "" && b8.getText() != ""
+                && b9.getText() != "") {
+            try{
+                Toast.makeText(MainActivity.this, "Game Draw", Toast.LENGTH_LONG).show();
+                Thread.sleep(1000);
+            }catch (Exception el){
+
+            }
+            b1.setText("");
+            b2.setText("");
+            b3.setText("");
+            b4.setText("");
+            b5.setText("");
+            b6.setText("");
+            b7.setText("");
+            b8.setText("");
+            b9.setText("");
+            b1.setEnabled(true);
+            b2.setEnabled(true);
+            b3.setEnabled(true);
+            b4.setEnabled(true);
+            b5.setEnabled(true);
+            b6.setEnabled(true);
+            b7.setEnabled(true);
+            b8.setEnabled(true);
+            b9.setEnabled(true);
         }
+    }
+    public void resetAllButton(){
+        b1.setText("");
+        b2.setText("");
+        b3.setText("");
+        b4.setText("");
+        b5.setText("");
+        b6.setText("");
+        b7.setText("");
+        b8.setText("");
+        b9.setText("");
     }
 }
